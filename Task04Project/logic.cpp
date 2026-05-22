38c19d2872a5b8313c3dbec2f2a968e82641a107
Dmitry <dmitrysamisenko@gmail.com>
2026-05-22 18:31:00 +0300

    task02

--- Task04Project/logic.cpp
+++ Task04Project/logic.cpp
@@ -3,3 +3,15 @@
 // 
 // Необходимо спроектировать и реализовать программу, 
 // которая возвращает сумму кубов чисел от 1 до N.
+#include "logic.h"
+int recursion(int number) {
+	if (number == 1){
+	return 1;
+	}
+
+	return recursion(number )
+
+}
+int sum_of_cubes(int number) {
+
+}
