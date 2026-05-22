ec95ffc849031e771c3af28af134067b3824a9a9
Dmitry <dmitrysamisenko@gmail.com>
2026-05-22 19:37:59 +0300

    task06

--- Task06Project/logic.cpp
+++ Task06Project/logic.cpp
@@ -4,17 +4,28 @@
 // Необходимо спроектировать и реализовать программу, 
 // которая вычисляет сумму чётных чисел от 1 до N.
 #include "logic.h"
+
 int recursion(int number) {
-	if (number <= 0) {
+	if (number == 0) {
+		return 0;
+	}
+	
+	return recursion(number - 2) + number;
+}
+
+int sum(int number) {
+	if (number % 2 != 0) {
+		number--;
+	}
+
+	if (number < 0) {
 		return -1;
 	}
-	if (number % 2 == 0) {
-		return number + recursion(number - 1);
-	}
 
-	return recursion(number - 1);
-}
-int sum(int number) {
+	// 8--> 8 + 6 + 4 + 2 0
+	// 7 -- 6
+	
 
 	return recursion(number);
+	
 }
