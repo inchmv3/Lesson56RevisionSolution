5965dcf9c64558eb8cc4bad3f1722c11057133f1
Dmitry <dmitrysamisenko@gmail.com>
2026-05-22 18:14:48 +0300

    task03

--- Task03Project/logic.cpp
+++ Task03Project/logic.cpp
@@ -3,3 +3,21 @@
 // 
 // Необходимо спроектировать и реализовать программу, 
 // которая вычисляет сумму чисел от N до M.
+#include "logic.h"
+int recursion(int n, int m) {
+	if (m == n) {
+		return n;
+	}
+
+	return recursion(n, m - 1) + m;
+}
+int sum(int n, int m) {
+	if (n > m) {
+		int t = n;
+		n = m;
+		m = t;
+	}
+
+	return recursion(n, m);
+
+}
